91fd9c5741c549969b8821c61ff9240678155993
AkshaySG <akshay.sg55@gmailcom>
2024-12-06 00:55:05 +0000

    finishing function add flight and utils

--- add_flight.py
+++ add_flight.py
@@ -2,7 +2,7 @@ import inquirer
 import os
 import sys
 import sqlite3
-from utils import fetch_data_single
+from utils import fetch_data_single, check_if_is_empty,flight_table_validation, destination_table_validation, pilot_table_validation
 import re
 
 
@@ -37,27 +37,12 @@ def create_from_scratch(con):
     is_not_empty = False
     answers = inquirer.prompt(ques)
     flight_ids = fetch_data_single("flight", "flight_id", cur)
-    print(flight_ids)
-    for _, val in  answers.items():
-        if val is None or val.strip() == '':
-            error_name ="No input was recorded in one or all of the prompts. Returning to the menu..."
-            is_not_empty = True
-            break
+    is_not_empty, error_name = check_if_is_empty(answers, error_name)
     if not is_not_empty:
         is_valid = True
         flight_id = answers["flight_id"]
         flight_name = answers["flight_name"]
-        number_pattern = r"^-?\d+(\.\d+)?$"
-        if not re.match(number_pattern, flight_id):
-            is_valid = True
-        error_name = 'Enter a number for flight id'
-    
-        val_int = int(flight_id.strip())
-        if val_int in flight_ids:
-            error_name = f"Flight ID {flight_id} already exists. Please choose another ID."
-            is_valid = False
-            
-    
+        is_valid, error_name = flight_table_validation(flight_id, flight_name, is_valid, error_name, flight_ids)
         if not is_valid:
             print(f"{error_name}")
         else:
@@ -72,18 +57,161 @@ def create_from_scratch(con):
     else:
         print(f"{error_name}")
             
-            
-            
+        
+def get_schedule_id(cur):
+    cur.execute('SELECT MAX(schedule_id) FROM schedule')
+    res = cur.fetchone()[0]
+    if res:
+        prefix = res[:3]
+        number = int(res[3:])
+        next_number = number + 1
+        return f"{prefix}{next_number:03}"
+    else:
+        return "SCH101"
     
     
 
-def flight_viewing():
-    return 0
 
+def write_in_db(option_a, tables, con, cur, flight_id, flight_name, destination_id,time,
+                date, from_location, to_location,pilot_id, pilot_name):
+    for table in tables:
+        if table == 'flight':
+            query = "INSERT INTO flight (flight_id, flight_name) VALUES (?, ?);"
+            cur.execute(query, (flight_id, flight_name))
+        elif table == 'pilot' and not option_a:
+                query = "INSERT INTO pilot (pilot_id, pilot_name) VALUES (?, ?);"
+                cur.execute(query, (pilot_id, pilot_name))
+        elif table == 'destination':
+                query = """INSERT INTO destination (flight_id, destination_id, time, date, from_location, to_location) 
+                        VALUES (?, ? , ?, ?, ?, ?);
+                         """ 
+                cur.execute(query, (flight_id, destination_id, time, date, from_location, to_location))
+        elif table == 'schedule':
+                schedule_id = get_schedule_id(cur)
+                print(f"Schedule id: {schedule_id}")
+                query = "INSERT INTO schedule (schedule_id, flight_id, destination_id, pilot_id) VALUES (?, ?, ?, ?)"
+                cur.execute(query, (schedule_id, flight_id, destination_id, pilot_id))
+    
+    con.commit()
+    print("Data successfully added to the table")
+    
+            
+def create_along_with_route(con):
+    cur = con.cursor()
+    choices_arr = [("Choose pilots from pre-existing list","a"),
+                   ("Create by entering a new pilot Name","b")]
 
-def flight_info_updation():
-    return 0
+    sub_menu_options = [
+        inquirer.List("option",
+             message="Choose Pilots from preexisting list",
+             choices= choices_arr,),
+    ]
+    options = inquirer.prompt(sub_menu_options)
+    while True:
+        selected_optn = options["option"]
+        if selected_optn == 'a':
+            print("Fetching unassigned pilot names")
+            cur.execute("""SELECT p.pilot_name, p.pilot_id FROM pilot p LEFT JOIN 
+                        schedule s ON p.pilot_id = s.pilot_id WHERE s.schedule_id IS NULL;""")
+            res = cur.fetchall()
+            print(f"Result {res}")
+            pilot_dict = {pilot[0]: pilot[1] for pilot in res}
+            options = [inquirer.List('selected_name', message="Select a pilot name", choices=list(pilot_dict.keys()))]
+            selec_pilots = inquirer.prompt(options)
+            if selec_pilots:
+                selected_pilot_name = selec_pilots['selected_name']
+                selected_pilot_id = pilot_dict[selected_pilot_name]
+                ques = [
+                inquirer.Text('flight_id', message="Enter a new Flight Id",),
+                inquirer.Text('flight_name', message="Enter the flight name"),
+                inquirer.Text('destination_id', message="Enter a new Route Id",),
+                inquirer.Text('time', message="Enter the time in format HH:MM AM/PM"),
+                inquirer.Text('date', message="Enter the date in YYYY-MM-DD format",),
+                inquirer.Text('from_location', message="Enter the location from where the flight should take place"),
+                inquirer.Text('to_location', message="Enter the flight destination",),
+                 ]
+                
+            answers = inquirer.prompt(ques)
+            is_not_empty, error_name = check_if_is_empty(answers, error_name)
+            if not is_not_empty:
+                sub_errname = ''
+                is_valid = True
+                flight_ids = fetch_data_single("flight", "flight_id", cur)
+                destination_ids = fetch_data_single('destination', "destination_id", cur)
+                flight_id = answers["flight_id"]
+                flight_name = answers["flight_name"]
+                destination_id = answers ["destination_id"]
+                time = answers["time"]
+                date = answers["date"]
+                from_location = answers["from_location"]
+                to_location = answers["to_location"]
+                is_valid, sub_errname = flight_table_validation(flight_id, flight_name, is_valid, sub_errname, flight_ids)
+                is_valid, sub_errname = destination_table_validation(destination_id,time, date, destination_ids, is_valid, sub_errname)
 
+                if not is_valid:
+                    print(f"{sub_errname}")
+                else:
+                    try:
+                        tables = ['flight', 'destination', 'schedule']
+                        write_in_db(True, tables, con, cur, flight_id, flight_name, destination_id,time,
+                                    date, from_location, to_location, selected_pilot_id)
+                        break
+                    except sqlite3.Error as e:
+                        print(f"Error in writng data to the table: {e}")
+                        con.rollback()
+            
+            break
+        elif selected_optn == 'b':
+            error_name = ''
+            is_not_empty = False
+            ques = [
+                inquirer.Text('flight_id', message="Enter a new Flight Id",),
+                inquirer.Text('flight_name', message="Enter the flight name"),
+                inquirer.Text('pilot_id', message="Enter a new Pilot id",),
+                inquirer.Text('pilot_name', message="Enter the Pilot name"),
+                inquirer.Text('destination_id', message="Enter a new Route Id",),
+                inquirer.Text('time', message="Enter the time in format HH:MM AM/PM"),
+                inquirer.Text('date', message="Enter the date in YYYY-MM-DD format",),
+                inquirer.Text('from_location', message="Enter the location from where the flight should take place"),
+                inquirer.Text('to_location', message="Enter the flight destination",),
+                 ]
+            answers = inquirer.prompt(ques)
+            is_not_empty, error_name = check_if_is_empty(answers, error_name)
+            if not is_not_empty:
+                sub_errname = ''
+                is_valid = True
+                flight_ids = fetch_data_single("flight", "flight_id", cur)
+                pilot_ids = fetch_data_single('pilot', "pilot_id", cur)
+                destination_ids = fetch_data_single('destination', "destination_id", cur)
+                flight_id = answers["flight_id"]
+                flight_name = answers["flight_name"]
+                pilot_id = answers["pilot_id"]
+                pilot_name = answers["pilot_name"]
+                destination_id = answers ["destination_id"]
+                time = answers["time"]
+                date = answers["date"]
+                from_location = answers["from_location"]
+                to_location = answers["to_location"]
+                is_valid, sub_errname = flight_table_validation(flight_id, flight_name, is_valid, sub_errname, flight_ids)
+                is_valid, sub_errname = pilot_table_validation(pilot_id, pilot_name, is_valid, sub_errname, pilot_ids)
+                is_valid, sub_errname = destination_table_validation(destination_id,time, date, destination_ids, is_valid, sub_errname)
+                if not is_valid:
+                    print(f"{sub_errname}")
+                else:
+                    try:
+                        tables = ['flight', 'pilot', 'destination', 'schedule']
+                        write_in_db(False, tables, con, cur, flight_id, flight_name, destination_id,time,
+                                    date, from_location, to_location,pilot_id, pilot_name)
+                        break
+                    except sqlite3.Error as e:
+                        print(f"Error in writng data to the table: {e}")
+                        con.rollback()
+            else:
+                print(f"{error_name}")
+        else:
+            print("Something went wrong....")
+    
+    
 
 
 def flight_addition(con):
@@ -93,7 +221,7 @@ def flight_addition(con):
         if flight_addition_menu_optn == "a":
             create_from_scratch(con)
         elif flight_addition_menu_optn == "b":
-            flight_viewing(con)
+            create_along_with_route(con)
         elif flight_addition_menu_optn == 'c':
             print("Returning to main menu...")
             break
--- utils.py
+++ utils.py
@@ -1,11 +1,60 @@
-
+import re
 
 def fetch_data_single(table_name, column_name, cur):
     cur.execute(f"SELECT {column_name} FROM {table_name}")
     col_elements =[]
     flight_id_cols = cur.fetchall()
     for flight_id_col in flight_id_cols:
-        print(f"type of olfnskfnak: {type(flight_id_col[0])}")
         col_elements.append(flight_id_col[0])
     return col_elements
     
+def check_if_is_empty(answers, error_name):
+    is_not_empty = False
+    for _, val in  answers.items():
+        if val is None or val.strip() == '':
+            error_name ="No input was recorded in one or all of the prompts. Returning to the menu..."
+            is_not_empty = True
+    return is_not_empty, error_name
+
+
+def flight_table_validation(flight_id, flight_name, is_valid, error_name, flight_ids):
+    number_pattern = r"^(?:10[1-9]|[1-9]\d{3,}|\d{3,})$"
+    if not re.match(number_pattern, flight_id):
+        is_valid = False
+        error_name = 'Enter a number for flight id and it should be greater than 100'
+    
+    val_int = int(flight_id.strip())
+    if val_int in flight_ids:
+        error_name = f"Flight ID {flight_id} already exists. Please choose another ID."
+        is_valid = False
+    return is_valid, error_name
+
+
+def pilot_table_validation(pilot_id, pilot_name, is_valid, sub_errname, pilot_ids):
+    number_pattern = r"^(?:100[1-9]|10[1-9]\d|1[1-9]\d{2}|\d{4,})$"
+    if not re.match(number_pattern, pilot_id):
+        is_valid = False
+        sub_errname = "Enter pilot id in number format and it should be greater than 1000"
+    int_val = int(pilot_id.strip())
+    if int_val in pilot_ids:
+        sub_errname = f"Flight ID {pilot_id} already exists. Please choose another ID."
+        is_valid = False
+    return is_valid, sub_errname
+
+
+def destination_table_validation(destination_id,time, date, destination_ids, is_valid, sub_errname):
+    dest_id_pattern = r"^UK\w*$"
+    time_pattern = r"^(0[1-9]|1[0-2]):[0-5][0-9](:[0-5][0-9])?\s?(AM|PM)$"
+    date_pattern = r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])$"
+    if not re.match(dest_id_pattern, destination_id):
+        sub_errname = "Destination ID needs to start with UK.. as per company policy"
+    elif not re.match(time_pattern, time):
+        is_valid = False
+        sub_errname = "Time should be in the format of HH:MM:SS AM/PM"
+    elif not re.match(date_pattern, date):
+        is_valid =False
+        sub_errname = "Date should be in the format of YYYY-MM-DD"
+    if destination_id in destination_ids:
+        sub_errname = f"Flight ID {destination_id} already exists. Please choose another ID."
+        is_valid = False
+    return is_valid, sub_errname
